a2195cbe7f22c55de88789d0101c848fd33fd533
Artem Fedorov <artem.fedorov@blazemeter.com>
2018-07-04 11:50:14 +0300

    init

---
+++ src/main/java/com/blazemeter/taurus/junit/Utils.java
@@ -0,0 +1,13 @@
+package com.blazemeter.taurus.junit;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class Utils {
+    public static String getStackTrace(final Throwable throwable) {
+        final StringWriter sw = new StringWriter();
+        final PrintWriter pw = new PrintWriter(sw, true);
+        throwable.printStackTrace(pw);
+        return sw.getBuffer().toString();
+    }
+}
