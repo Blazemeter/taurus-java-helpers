dc860b1aa584a8af3b1e7edbe206f6db0959b7af
Artem Fedorov <artem.fedorov@blazemeter.com>
2018-07-07 15:05:44 +0300

    fix new listener

--- src/main/java/com/blazemeter/taurus/junit5/JUnit5Listener.java
+++ src/main/java/com/blazemeter/taurus/junit5/JUnit5Listener.java
@@ -3,11 +3,15 @@ package com.blazemeter.taurus.junit5;
 import com.blazemeter.taurus.junit.CustomListener;
 import com.blazemeter.taurus.junit.Sample;
 import com.blazemeter.taurus.junit.TaurusReporter;
+import com.blazemeter.taurus.junit.Utils;
 import org.junit.platform.engine.TestExecutionResult;
+import org.junit.platform.engine.TestSource;
+import org.junit.platform.engine.support.descriptor.MethodSource;
 import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestIdentifier;
 import org.junit.platform.launcher.TestPlan;
 
+import java.util.Optional;
 import java.util.logging.Logger;
 
 public class JUnit5Listener implements TestExecutionListener {
@@ -32,35 +36,96 @@ public class JUnit5Listener implements TestExecutionListener {
     @Override
     public void testPlanExecutionFinished(TestPlan testPlan) {
         log.info("Test Plan Finished");
+        log.info("Run Finished, successful=" + (failedCount == 0) +", run count=" + testCount);
+
     }
 
     @Override
     public void executionStarted(TestIdentifier testIdentifier) {
-        log.info(String.format("started %s", testIdentifier.getDisplayName()));
+        if (testIdentifier.isTest()) {
+            startSample(testIdentifier);
+            testCount += 1;
+        }
+    }
+
+    protected void startSample(TestIdentifier testIdentifier) {
+        Optional<TestSource> source = testIdentifier.getSource();
+        if (source.isPresent()) {
+            TestSource testSource = testIdentifier.getSource().get();
+            if (testSource instanceof MethodSource) {
+                startSample((MethodSource) testSource);
+            } else {
+                //TODO: other test source..
+            }
+        }
+    }
+
+    protected void startSample(MethodSource source) {
+        log.info(String.format("started %s(%s)", source.getMethodName(), source.getClassName()));
         started = System.currentTimeMillis();
         pendingSample = new Sample();
-//        pendingSample.setLabel(testIdentifier.getMethodName());
-//        pendingSample.setSuite(description.getClassName());
-//        pendingSample.setFullName(description.getClassName() + "." + description.getMethodName());
-        testCount += 1;
+        pendingSample.setLabel(source.getMethodName());
+        pendingSample.setSuite(source.getClassName());
+        pendingSample.setFullName(source.getClassName() + "." + source.getMethodName());
     }
 
     @Override
     public void executionFinished(TestIdentifier testIdentifier, TestExecutionResult testExecutionResult) {
-        log.info(String.format("finished %s", testIdentifier.getDisplayName()));
+        if (testIdentifier.isTest()) {
+            String status = getStatus(testExecutionResult.getStatus());
+            Optional<Throwable> optional = testExecutionResult.getThrowable();
+            if (optional.isPresent()) {
+                Throwable throwable = optional.get();
+                finishSample(status, throwable.getMessage(), throwable);
+            } else {
+                finishSample(status, null, null);
+            }
+        }
+    }
+
+    private String getStatus(TestExecutionResult.Status status) {
+        switch (status) {
+            case SUCCESSFUL:
+                return Sample.STATUS_PASSED;
+            case FAILED:
+                return Sample.STATUS_FAILED;
+            case ABORTED:
+                return Sample.STATUS_BROKEN;
+                default:
+                    return Sample.STATUS_FAILED;
+        }
+    }
+
+    //todo: print log about fails/abort etc..
+    private void finishSample(String status, String msg, Throwable ex) {
+        log.info(String.format("finished %s(%s)", pendingSample.getLabel(), pendingSample.getSuite()));
         double duration = (System.currentTimeMillis() - started) / 1000.0;
         pendingSample.setDuration(duration);
+        pendingSample.setStatus(status);
+        pendingSample.setErrorMessage(msg);
+        if (ex != null) {
+            pendingSample.setErrorTrace(Utils.getStackTrace(ex));
+        }
+
         reporter.writeSample(pendingSample);
 
         if (!pendingSample.isSuccessful()) {
             failedCount += 1;
         }
+        System.out.printf(report_tmpl,
+                pendingSample.getSuite(),
+                pendingSample.getLabel(),
+                testCount,
+                testCount - failedCount,
+                failedCount);
         pendingSample = null;
-//        System.out.printf(report_tmpl,
-//                description.getClassName(),
-//                description.getMethodName(),
-//                testCount,
-//                testCount - failedCount,
-//                failedCount);
     }
+
+    @Override
+    public void executionSkipped(TestIdentifier testIdentifier, String reason) {
+        startSample(testIdentifier);
+        finishSample(Sample.STATUS_SKIPPED, reason, null);
+    }
+
+
 }
