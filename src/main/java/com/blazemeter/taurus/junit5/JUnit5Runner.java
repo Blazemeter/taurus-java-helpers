760aa2391d9bc46d2c13cb6d5ecfa20defd992cc
Artem Fedorov <artem.fedorov@blazemeter.com>
2018-07-07 16:59:33 +0300

    add hold & iterations for junit 5

--- src/main/java/com/blazemeter/taurus/junit5/JUnit5Runner.java
+++ src/main/java/com/blazemeter/taurus/junit5/JUnit5Runner.java
@@ -5,20 +5,20 @@ import org.junit.platform.engine.DiscoverySelector;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
 import org.junit.platform.launcher.TestExecutionListener;
-import org.junit.platform.launcher.TestPlan;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
-import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.logging.Logger;
 
-import static org.junit.platform.engine.discovery.ClassNameFilter.includeClassNamePatterns;
+import static com.blazemeter.taurus.junit.CustomRunner.HOLD;
+import static com.blazemeter.taurus.junit.CustomRunner.ITERATIONS;
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
-import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
 
 public class JUnit5Runner {
+    private static final Logger log = Logger.getLogger(JUnit5Runner.class.getName());
 
     public static void run(ArrayList<Class> classes, Properties props, TaurusReporter reporter) throws Exception {
 
@@ -37,15 +37,29 @@ public class JUnit5Runner {
                 .build();
 
         Launcher launcher = LauncherFactory.create();
-
-// Register a listener of your choice
-        TestExecutionListener summaryGeneratingListener = new SummaryGeneratingListener();
         TestExecutionListener jUnit5Listener = new JUnit5Listener(reporter);
-
-        launcher.registerTestExecutionListeners(summaryGeneratingListener);
         launcher.registerTestExecutionListeners(jUnit5Listener);
 
-        launcher.execute(request);
+        long iterations = Long.valueOf(props.getProperty(ITERATIONS, "0"));
+        float hold = Float.valueOf(props.getProperty(HOLD, "0"));
+        if (iterations == 0) {
+            if (hold > 0) {
+                iterations = Long.MAX_VALUE;
+            } else {
+                iterations = 1;
+            }
+        }
+
+        long startTime = System.currentTimeMillis();
+        for (int iteration = 0; iteration < iterations; iteration++) {
+            launcher.execute(request);
+            log.info("Elapsed: " + (System.currentTimeMillis() - startTime) + ", limit: " + (hold * 1000));
+            if (hold > 0 && System.currentTimeMillis() - startTime > hold * 1000) {
+                log.info("Duration limit reached, stopping");
+                break;
+            }
+        }
+
 
         reporter.close();
     }
