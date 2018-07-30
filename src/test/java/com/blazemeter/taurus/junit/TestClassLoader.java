328539abe7bd919180fa618c19c38876b2eba31e
Artem Fedorov <artem.fedorov@blazemeter.com>
2018-07-30 18:03:39 +0300

    add test classloader

---
+++ src/test/java/com/blazemeter/taurus/junit/TestClassLoader.java
@@ -0,0 +1,34 @@
+package com.blazemeter.taurus.junit;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestClassLoader extends URLClassLoader {
+    public TestClassLoader(URL[] urls, ClassLoader parent) {
+        super(urls, parent);
+    }
+
+    public TestClassLoader(URL[] urls) {
+        super(urls);
+    }
+
+    public static URL[] getTestJars() {
+        List<URL> urls = new ArrayList<>();
+        urls.add(TestClassLoader.class.getResource("/junit-test-1.1.jar"));
+        urls.add(TestClassLoader.class.getResource("/dummyJUnit.jar"));
+        urls.add(TestClassLoader.class.getResource("/dummyTestNG.jar"));
+        urls.add(TestClassLoader.class.getResource("/empty.jar"));
+        return urls.toArray(new URL[0]);
+    }
+
+    public static TestClassLoader getTestClassLoaderWithSystemCL() {
+        return new TestClassLoader(getTestJars(), ClassLoader.getSystemClassLoader());
+    }
+
+    public static TestClassLoader getTestClassLoaderWithoutSystemCL() {
+        return new TestClassLoader(getTestJars());
+    }
+}
