98028603593d1dd3ff74d5283ee5decd59ba6110
Artem Fedorov <artem.fedorov@blazemeter.com>
2018-08-01 10:35:45 +0300

    add tests for filters

---
+++ src/test/java/com/blazemeter/taurus/junit/runner/junit5/JUnit5ClassFilterTest.java
@@ -0,0 +1,20 @@
+package com.blazemeter.taurus.junit.runner.junit5;
+
+import com.blazemeter.taurus.junit.demotests.EmptyTestClass;
+import com.blazemeter.taurus.junit.demotests.JUnit3Test;
+import com.blazemeter.taurus.junit.demotests.JUnit4Test;
+import com.blazemeter.taurus.junit.demotests.JUnit5Test;
+import com.blazemeter.taurus.junit.demotests.NoTestClass;
+import junit.framework.TestCase;
+
+public class JUnit5ClassFilterTest extends TestCase {
+
+    public void testFlow() {
+        JUnit5ClassFilter filter = new JUnit5ClassFilter();
+        assertTrue(filter.shouldAdd(EmptyTestClass.class));
+        assertTrue(filter.shouldAdd(JUnit3Test.class));
+        assertTrue(filter.shouldAdd(JUnit4Test.class));
+        assertTrue(filter.shouldAdd(JUnit5Test.class));
+        assertFalse(filter.shouldAdd(NoTestClass.class));
+    }
+}
